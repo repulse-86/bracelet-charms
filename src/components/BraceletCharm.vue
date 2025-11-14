fb2b3cf867c213a15ede895d1edee990d6bdc6d4
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-11-14 13:17:24 +0800

    feat: create a reusable BraceletCharm component

---
+++ src/components/BraceletCharm.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Metal, MetalColor } from '@/types/metal'
+import { computed } from 'vue';
+
+const props = defineProps<{ metal: Metal }>()
+const color = computed(() => MetalColor[props.metal])
+</script>
+
+<template>
+    <div :class="color" class="w-40 h-35 border border-gray-black">
+        {{ metal }}
+    </div>
+</template>
