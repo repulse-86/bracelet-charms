34751888c6ec4a02ad0d11b2de074faf6c878158
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-11-14 13:09:31 +0800

    style: set indent to 4

--- eslint.config.ts
+++ eslint.config.ts
@@ -12,6 +12,10 @@ export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: {
+      'vue/html-indent': ['error', 4],
+      '@typescript-eslint/indent': ['error', 4],
+    },
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
