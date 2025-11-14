ced80ab5c29a2233ac7ffc3d41c99487fcee9d8d
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-11-14 15:08:43 +0800

    feat: add bracelet preview to home page

---
+++ src/components/BraceletPreviewSection.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import BraceletCharm from './BraceletCharm.vue'
+import { useMetalSwitchStore } from '@/stores/metal-switcher'
+import { useBraceletMaker } from '@/stores/bracelet-maker'
+import { charms } from '@/data/charms'
+
+const metalSwitchStore = useMetalSwitchStore()
+const braceletMakerStore = useBraceletMaker()
+</script>
+
+<template>
+    <section>
+        <div class="bg-gray-50 p-4 flex justify-center items-center space-x-2">
+            <template
+                v-if="
+                    charms && charms.length > 0
+                "
+            >
+                <BraceletCharm
+                    v-for="charm in charms"
+                    :key="charm.id"
+                    :charm="charm"
+                    :metal="metalSwitchStore.selectedMetal"
+                    class="w-30 h-auto"
+                />
+            </template>
+            <p v-else class="text-8xl font-serif uppercase text-center">bracelet preview</p>
+        </div>
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import BaseColorSwitcherSection from '@/components/BaseColorSwitcherSection.vue'
+import BraceletPreviewSection from '@/components/BraceletPreviewSection.vue'
 import CharmsContainerSection from '@/components/CharmsContainerSection.vue'
 import HeaderSection from '@/components/HeaderSection.vue'
 </script>
@@ -8,6 +9,7 @@ import HeaderSection from '@/components/HeaderSection.vue'
     <div class="space-y-8">
         <HeaderSection />
         <BaseColorSwitcherSection />
+        <BraceletPreviewSection />
         <CharmsContainerSection />
     </div>
 </template>
