8a7547ce7c9ec2d75b0e3105f0b66b465ea1c233
repulse-86 <156637470+repulse-86@users.noreply.github.com>
2025-11-14 13:18:52 +0800

    feat: add a base color switcher section to home page

---
+++ src/components/BaseColorSwitcherSection.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Metal } from '@/types/metal'
+import BraceletCharm from '@/components/BraceletCharm.vue'
+import { useMetalSwitchStore } from '@/stores/metal-switcher'
+
+const metalSwitchStore = useMetalSwitchStore()
+</script>
+
+<template>
+    <section class="flex flex-col justify-center items-center">
+        <div class="flex justify-center space-x-4 mb-4">
+            <div class="flex flex-col">
+                <label for="" class="text-center text-4xl font-bold font-serif uppercase"
+                    >Silver</label
+                >
+                <BraceletCharm
+                    class="w-40 h-35"
+                    @click="metalSwitchStore.setMetal(Metal.Silver)"
+                    :metal="Metal.Silver"
+                />
+            </div>
+            <div class="flex flex-col">
+                <label for="" class="text-center text-4xl font-bold font-serif uppercase"
+                    >Gold</label
+                >
+                <BraceletCharm
+                    class="w-40 h-35"
+                    @click="metalSwitchStore.setMetal(Metal.Gold)"
+                    :metal="Metal.Gold"
+                />
+            </div>
+        </div>
+        <h2 class="text-lg uppercase font-serif">start your creation by picking a base color</h2>
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import BaseColorSwitcherSection from '@/components/BaseColorSwitcherSection.vue'
 import HeaderSection from '@/components/HeaderSection.vue'
 </script>
 
 <template>
     <div class="space-y-8">
         <HeaderSection />
+        <BaseColorSwitcherSection />
     </div>
 </template>
